f533fb0c70f915e5354f702fcc0cf1e0f1253751
GaliyaNass <nasmessage@gmail.com>
2018-01-30 11:30:34 -0500

    added player movement

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //PUBLIC PROPERTIES
+    public float LeftBoundary;
+    public float RightBoundary;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+
+        Vector3 mousePosition = new Vector3(mouseX, -200f, 0f);
+
+        if (mousePosition.x > this.RightBoundary)
+        {
+            mousePosition.x = this.RightBoundary;
+        }
+
+        if (mousePosition.x < this.LeftBoundary)
+        {
+            mousePosition.x = this.LeftBoundary;
+        }
+
+        //every frame set the Player's position to the mouse position
+        transform.position = mousePosition; 
+	}
+}
